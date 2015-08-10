652d5361cbf56268b1049da614318491139b9c97
Matthieu Napoli <matthieu@mnapoli.fr>
2015-08-10 13:38:01 +0200

    Try to fix Travis script for git-lfs by reverting to v0.5.3

--- install_git_lfs.sh
+++ install_git_lfs.sh
@@ -11,14 +11,8 @@ then
     # TODO remove the $GITHUB_USER_TOKEN once v0.6.0 is released
     git remote set-url origin "https://$GITHUB_USER_TOKEN:@github.com/$TRAVIS_REPO_SLUG.git"
 
-    curl -sLo - https://github.com/github/git-lfs/releases/download/v0.5.4/git-lfs-linux-amd64-0.5.4.tar.gz | tar xzvf -
+    curl -sLo - https://github.com/github/git-lfs/releases/download/v0.5.3/git-lfs-linux-amd64-0.5.3.tar.gz | tar xzvf -
     cd git-lfs-*
-
-    # We overwrite the shebang of the file waiting for a new git-lfs release which
-    # contains https://github.com/github/git-lfs/commit/dbf09f71735e0456c7a707bba634016f66dfb1f7
-    # TODO remove once a version > 0.5.4 is released
-    sed "1s/.*/\#\!\/usr\/bin\/env bash/" install.sh > install.sh
-    cat install.sh
 
     sudo ./install.sh
     cd ..
