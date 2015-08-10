07404710e732f91752ada2f22f6f98105fe35ea2
Matthieu Napoli <matthieu@mnapoli.fr>
2015-08-10 13:46:10 +0200

    Try to fix Travis script for git-lfs by reverting to v0.5.3

--- install_git_lfs.sh
+++ install_git_lfs.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -e
-
 # Install git lfs for UI tests screenshots
 # TODO: remove/update when Travis updates the VM (should be installed by default)
 if [ "$TEST_SUITE" = "UITests" ];
