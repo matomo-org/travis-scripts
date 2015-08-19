bfb563a84577224f17f56a67799116d1b2530acb
Matthieu Napoli <matthieu@mnapoli.fr>
2015-08-19 16:18:38 +0200

    Try to compile git-lfs from source

--- install_git_lfs.sh
+++ install_git_lfs.sh
@@ -5,16 +5,33 @@
 if [ "$TEST_SUITE" = "UITests" ];
 then
 
-    # Change the remote because git lfs doesn't support git:// URLs
+    # Change the remote because git lfs doesn't support git:// URLs for now
     # TODO remove the $GITHUB_USER_TOKEN once v0.6.0 is released
     git remote set-url origin "https://$GITHUB_USER_TOKEN:@github.com/$TRAVIS_REPO_SLUG.git"
 
-    curl -sLo - https://github.com/github/git-lfs/releases/download/v0.5.4/git-lfs-linux-amd64-0.5.4.tar.gz | tar xzvf -
-    cd git-lfs-*
+    go_version=$(go version | sed 's/go version go//; s/ .*//')
+    echo $go_version
+    if [[ $go_version < 1.3.1 ]]; then
+        [[ -r ~/.gvm/scripts/gvm ]] ||
+            bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
+        set +u
+        source ~/.gvm/scripts/gvm
+        gvm install go1.4.2
+        gvm use go1.4.2
+        set -u
+    fi
 
-    sudo ./install.sh
-    cd ..
-    rm -rf git-lfs-*
+    CWD=$(pwd)
+
+    cd /tmp
+    git clone https://github.com/github/git-lfs
+    cd git-lfs
+
+    ./script/bootstrap
+    sudo install -D bin/git-lfs /usr/local/bin
+    git lfs init
+
+    cd $CWD
 
     # Now that git-lfs is installed we download the screenshots
     git lfs fetch
