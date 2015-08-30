5aadbce155629d90e6615d8648b71c8eb234ae39
diosmosis <benaka@piwik.pro>
2015-08-30 12:59:22 -0700

    Add output before checking for travis-scripts branch.

--- checkout_test_against_branch.sh
+++ checkout_test_against_branch.sh
@@ -49,6 +49,7 @@ fi
 
 cd tests/travis
 
+echo "Checking for travis-scripts branch to use..."
 if git rev-parse --verify "$TEST_AGAINST_PIWIK_BRANCH" ; then
     echo "Found travis-scripts branch or tag corresponding to TEST_AGAINST_PIWIK_BRANCH environment variable, checking out '$TEST_AGAINST_PIWIK_BRANCH' for tests."
 
