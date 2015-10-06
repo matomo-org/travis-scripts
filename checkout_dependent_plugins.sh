940dad370dbabe33cfb0c49a0844866341f2062b
diosmosis <benaka@piwik.pro>
2015-10-06 06:01:28 -0700

    Debugging.

--- checkout_dependent_plugins.sh
+++ checkout_dependent_plugins.sh
@@ -13,6 +13,7 @@ else
         dependentPluginName=`echo "$pluginSlug" | sed -E 's/[a-zA-Z0-9_]+\/[a-zA-Z0-9_]+-(.*)/\1/'`
 
         echo "Cloning $pluginSlug into plugins/$dependentPluginName..."
+        echo "change"
         git clone --depth=1 "https://$GITHUB_USER_TOKEN:@github.com/$pluginSlug" "plugins/$dependentPluginName" 2>&1 | sed "s/$GITHUB_USER_TOKEN/\$GITHUB_USER_TOKEN/"
     done
 
