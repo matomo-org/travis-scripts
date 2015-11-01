87effd67cc7fce9cf3e1b1a85363ed0edbafe28c
diosmosis <benaka@piwik.pro>
2015-11-01 09:13:27 -0800

    Fix last commit, use TRAVIS_BUILD_DIR so we find the correct repo's .travis.yml.

--- setup_webserver.sh
+++ setup_webserver.sh
@@ -60,7 +60,7 @@ echo "Starting php-fpm"
 $PHP_FPM_BIN --fpm-config "$DIR/php-fpm.ini"
 
 echo "Starting nginx using config $DIR/piwik_nginx.conf"
-if grep "sudo: false" "$DIR/../../.travis.yml"; then
+if grep "sudo: false" "$TRAVIS_BUILD_DIR/.travis.yml"; then
     nginx -c "$DIR/piwik_nginx.conf"
 else
     # use port 80 if this build allows using sudo
