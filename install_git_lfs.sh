9402b285dd6f5cdfb68b7f5da2c7f38f30ea6022
Marc Neudert <marc.neudert@gmail.com>
2017-09-02 20:10:16 +0200

    git-lfs: 1.2.1 -> 2.2.1

--- install_git_lfs.sh
+++ install_git_lfs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-GIT_LFS_VERSION=1.2.1
+GIT_LFS_VERSION=2.2.1
 
 curl -sLo - https://github.com/github/git-lfs/releases/download/v${GIT_LFS_VERSION}/git-lfs-linux-amd64-${GIT_LFS_VERSION}.tar.gz | tar xzvf -
 
