119a2d75f87105f833b38f486efd346e315f1619
Marc Neudert <marc.neudert@gmail.com>
2017-09-03 11:06:16 +0200

    install git-lfs only is missing

--- install_git_lfs.sh
+++ install_git_lfs.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-GIT_LFS_VERSION=2.2.1
+command -v 'git-lfs' >/dev/null || {
+  GIT_LFS_VERSION=2.2.1
 
-curl -sLo - https://github.com/github/git-lfs/releases/download/v${GIT_LFS_VERSION}/git-lfs-linux-amd64-${GIT_LFS_VERSION}.tar.gz | tar xzvf -
+  curl -sLo - https://github.com/github/git-lfs/releases/download/v${GIT_LFS_VERSION}/git-lfs-linux-amd64-${GIT_LFS_VERSION}.tar.gz | tar xzvf -
 
-mkdir -p "${HOME}/bin"
-mv git-lfs-*/git-lfs "${HOME}/bin"
-rm -rf git-lfs-*
+  mkdir -p "${HOME}/bin"
+  mv git-lfs-*/git-lfs "${HOME}/bin"
+  rm -rf git-lfs-*
 
-export PATH="${HOME}/bin:$PATH"
+  export PATH="${HOME}/bin:$PATH"
+}
 
 if [[ "${TEST_SUITE}" == "UITests" ]]; then
     git lfs fetch
